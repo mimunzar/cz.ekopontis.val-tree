dd465f84332fa47893521c84bd65ff606eda8ea9
Milan Munzar <mimunzar@gmail.com>
2022-04-25 10:34:52 +0200

    Program Args

--- src/val_tree/valuate_sheet.py
+++ src/val_tree/valuate_sheet.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import argparse
 import sys
 
 import src.val_tree.gateways.measurements as measurements
@@ -8,14 +9,20 @@ import src.val_tree.libs.util as util
 import src.val_tree.libs.log  as log
 
 
+def parse_args(args_it):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('input-sheet', help='The input Excell sheet with measurements',
+            metavar='FILE')
+    parser.add_argument('--speed',     help='The number of valuated items per second',
+            required=False, metavar="NUM", default=2)
+    return vars(parser.parse_args(args_it))
+
 
 if '__main__' == __name__:
-    if 2 > len(sys.argv):
-        sys.stderr.write(log.fmt_err('Missing input file'))
-        sys.exit(1)
+    args = parse_args(sys.argv[1:])
 
     print(log.fmt_msg('Parsing tree sheet:\n'))
-    t_it, err_it = measurements.iter_tree_sheet(util.second(sys.argv))
+    t_it, err_it = measurements.iter_tree_sheet(args['input-sheet'])
     if (err_it):
         sys.stderr.write(log.fmt_err('Found errors in tree sheet:\n\n'))
         util.consume(map(sys.stderr.write, err_it))
